8b38dce6f8062248fbd9ffaf10c16ee196d12baf
priti-yadav <priti.yadav@Gmail.com>
2018-03-15 14:21:37 -0700

    Revert "Merge branch 'master' of https://github.com/Makishanuto/CSE360Proj"
    This reverts commit 9c096e63a15bb6508a96388ad82807f7a564376e, reversing
    changes made to 515b1f5b8b2be09f8f347a10074f448273774784.

--- sourceFiles/DisplayGUI.java
+++ sourceFiles/DisplayGUI.java
@@ -155,7 +155,7 @@ public class DisplayGUI extends JApplet
         			String combine = "%80s";
         			for (int i = 0; i < stringsForUse.size(); i++){
         				combine += stringsForUse.get(i) + "\n";
-        	        	area.append(combine);
+        	        	System.out.printf(combine);
         			}
                 }
             }
