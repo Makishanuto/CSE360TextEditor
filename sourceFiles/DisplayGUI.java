687f7ced33e5b5af38d8e3619b3fe79151d47850
Shreya Madaan <shreya.madaan09@gmail.com>
2018-03-15 10:53:03 -0700

    again

--- sourceFiles/DisplayGUI.java
+++ sourceFiles/DisplayGUI.java
@@ -79,7 +79,7 @@ public class DisplayGUI extends JApplet
                           int x;
                           for(int j = 0; j < stringsForUse.size(); j++){
                               x = stringsForUse[j].length();
-                              System.out.printf("-%" + x + "s", i);
+                              System.out.println("-%" + (80-x) + "s", i);
                           }
                       }
                   }
