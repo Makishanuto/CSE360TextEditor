bddde226f317cef0d4febd8f5391e95601c2b111
Matthew <f0wlf4ng@gmail.com>
2018-03-15 09:45:30 -0700

    Edit sourceFiles

--- sourceFiles/Main.java
+++ sourceFiles/Main.java
@@ -1,9 +1,9 @@
-import java.awt.*;  
+import java.awt.*;
 import javax.swing.*;
 
 import java.awt.event.*;
 import javax.swing.JApplet;
-import java.awt.*;  
+import java.awt.*;
 import javax.swing.*;
 
 import java.awt.event.*;
@@ -11,11 +11,21 @@ import javax.swing.JApplet;
 
 import java.io.*; // File
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main extends JApplet
 {
     public static void main(String args[])
     {
+      ArrayList<String> stringsForUse
+      try
+      {
+        Scanner userInput = new Scanner(System.in);
+
+
+        while(userInput.hasNextLine())
+      }
         new DisplayGUI();
     }
+
 }
